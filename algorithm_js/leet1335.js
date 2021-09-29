e089e6dae8266ae01ffcd56b6b842e47a4b67301
Jinwoo Lee <starday456@kaist.ac.kr>
2021-09-29 14:54:23 +0900

    (UNSOLVED) 1335 마무리

--- algorithm_js/leet1335.js
+++ algorithm_js/leet1335.js
@@ -1,5 +1,9 @@
+// UNSOLVED
 const assert = require("assert");
 
+/**
+ * DFS version
+ */
 let jobs = undefined;
 
 const minDifficultyDfs = (idx, day) => {
@@ -15,14 +19,39 @@ const minDifficultyDfs = (idx, day) => {
   return result;
 };
 
+// const minDifficulty = (jobDifficulty, d) => {
+//   if (jobDifficulty.length < d) return -1;
+
+//   jobs = jobDifficulty;
+//   return minDifficultyDfs(0, d);
+// };
+
+/**
+ * Stack version
+ */
+
 const minDifficulty = (jobDifficulty, d) => {
   if (jobDifficulty.length < d) return -1;
 
-  jobs = jobDifficulty;
-  return minDifficultyDfs(0, d);
+  let dp1 = new Array(jobDifficulty.length).fill(Number.MAX_VALUE);
+  let dp2 = new Array(jobDifficulty.length).fill(0);
+  for (let i = 0; i < d; i++) {
+    const stack = new Array();
+    for (let j = i; j < jobDifficulty.length; j++) {
+      dp2[j] = j ? dp1[j - 1] + jobDifficulty[j] : jobDifficulty[j];
+      while (stack.length && jobDifficulty[stack[stack.length - 1]] <= jobDifficulty[j]) {
+        const k = stack.splice(0, 1)[0];
+        dp2[j] = Math.min(dp2[j], dp2[k] - jobDifficulty[k] + jobDifficulty[j]);
+      }
+      if (stack.length) dp2[j] = Math.min(dp2[j], dp2[stack[stack.length - 1]]);
+      stack.splice(0, 0, j);
+    }
+    [dp1, dp2] = [dp2, dp1];
+  }
+  return dp1[dp1.length - 1];
 };
 
-assert.deepEqual(minDifficulty([9, 9, 9], 4), -1);
+// assert.deepEqual(minDifficulty([9, 9, 9], 4), -1);
 
 assert.deepEqual(minDifficulty([1, 1], 2), 2);
 assert.deepEqual(minDifficulty([1, 1, 1], 3), 3);
