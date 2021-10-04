12e5d2da8f2941fc0994fd988b72d656f9f11d27
Jinwoo Lee <starday456@kaist.ac.kr>
2021-10-04 14:17:52 +0900

    add TreeNode class

---
+++ algorithm_js/lib/TreeNode.js
@@ -0,0 +1,9 @@
+class TreeNode {
+  constructor(value = undefined, left = undefined, right = undefined) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+module.exports = { TreeNode };
