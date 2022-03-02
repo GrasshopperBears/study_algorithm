ced3ec0c0ab6fe648c88de2a4cf7529f582ca213
Jinwoo Lee <starday456@kaist.ac.kr>
2022-03-02 16:42:38 +0900

    improve memory usage

--- algorithm_js/leet516.ts
+++ algorithm_js/leet516.ts
@@ -1,29 +1,28 @@
 const assert = require('assert');
 
-const helper = (s: string, from: number, to: number, dp: number[][]): number => {
-  if (to - from < 0) return 0;
-  if (to - from === 0) return 1;
-  if (dp[from][to] >= 0) return dp[from][to];
-
-  if (s[from] === s[to]) {
-    const result = 2 + helper(s, from + 1, to - 1, dp);
-    dp[from][to] = result;
-    return result;
-  }
-
-  const result = Math.max(helper(s, from + 1, to, dp), helper(s, from, to - 1, dp));
-  dp[from][to] = result;
-  return result;
-};
-
 const longestPalindromeSubseq = (s: string): number => {
   const length = s.length;
-  const dp = new Array(length).fill(0).reduce((curr) => {
-    curr.push(new Array(length).fill(-1));
+  const dp = new Array(length).fill(0).reduce((curr, _, idx) => {
+    curr.push(new Array(idx + 1).fill(-1));
     return curr;
   }, []);
 
-  return helper(s, 0, length - 1, dp);
+  const helper = (s: string, from: number, to: number): number => {
+    let result;
+
+    if (to - from < 0) result = 0;
+    else if (to - from === 0) result = 1;
+    else if (dp[from][to] >= 0) return dp[from][to];
+    else if (s[from] === s[to]) result = 2 + helper(s, from + 1, to - 1);
+    else result = Math.max(helper(s, from + 1, to), helper(s, from, to - 1));
+
+    dp[from][to] = result;
+    return result;
+  };
+
+  helper(s, 0, length - 1);
+
+  return dp[0][length - 1];
 };
 
 assert.deepEqual(longestPalindromeSubseq('bbbab'), 4);
