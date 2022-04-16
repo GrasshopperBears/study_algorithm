4be5f43d7997e5a43cb5cf707e3a84961eb512d3
GrasshopperBears <starday456@naver.com>
2022-04-17 00:16:37 +0900

    improve 11: two pointer

--- algorithm_js/leet11.js
+++ algorithm_js/leet11.js
@@ -1,33 +1,21 @@
 const assert = require('assert');
 
 const maxArea = (heights) => {
-  const heightMap = new Map();
+  let left = 0,
+    right = heights.length - 1,
+    max = 0;
 
-  for (let i = 0; i < heights.length; i++) {
-    const height = heights[i];
-    if (!height) continue;
-    const prevList = heightMap.get(height);
-    heightMap.set(height, prevList ? [...prevList, i] : [i]);
+  while (left < right) {
+    const currentHeight = Math.min(heights[left], heights[right]);
+    max = Math.max(max, currentHeight * (right - left));
+    if (heights[right] > heights[left]) left++;
+    else right--;
   }
-
-  const heightKeys = [...heightMap.keys()];
-  heightKeys.sort((a, b) => b - a);
-
-  let min = -1,
-    max = -1,
-    result = 0;
-  for (let currentHeight of heightKeys) {
-    const indices = heightMap.get(currentHeight);
-    const first = indices[0];
-    const last = indices[indices.length - 1];
-    if (min < 0 || first < min) min = first;
-    if (max < 0 || last > max) max = last;
-    result = Math.max(result, currentHeight * (max - min));
-  }
-  return result;
+  return max;
 };
 
 assert.deepEqual(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]), 49);
 assert.deepEqual(maxArea([1, 1]), 1);
+assert.deepEqual(maxArea([1, 8, 6, 2, 5, 4, 8, 25, 7]), 49);
 
 console.log('All test passed');
