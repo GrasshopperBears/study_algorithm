35570cb442228765e37fa8880c6842946db09a4e
Jinwoo Lee <starday456@kaist.ac.kr>
2022-04-20 20:53:47 +0900

    solve leet 680

---
+++ algorithm_golang/leet680/leet680_test.go
@@ -0,0 +1,63 @@
+package leet680_test
+
+import (
+	// "fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func isPalindrome(s string) bool {
+	for left, right := 0, len(s) - 1; left < right; {
+		if s[left] != s[right] { return false }
+		left++
+		right--
+	}
+	return true
+}
+
+func validPalindrome(s string) bool {
+	if (len(s) <= 2) { return true }
+
+	for left, right := 0, len(s) - 1; left < right; {
+		if s[left] == s[right] {
+			left++
+			right--
+			continue
+		}
+		if isPalindrome(s[left + 1 : right + 1]) || isPalindrome(s[left : right]) { return true }
+		return false
+	}
+	return true
+}
+
+func TestValistPalindrome1(t *testing.T) {
+	result := validPalindrome("aba")
+	answer := true
+	assert.Equal(t, result, answer)
+}
+func TestValistPalindrome2(t *testing.T) {
+	result := validPalindrome("abca")
+	answer := true
+	assert.Equal(t, result, answer)
+}
+func TestValistPalindrome3(t *testing.T) {
+	result := validPalindrome("abc")
+	answer := false
+	assert.Equal(t, result, answer)
+}
+func TestValistPalindrome4(t *testing.T) {
+	result := validPalindrome("aa")
+	answer := true
+	assert.Equal(t, result, answer)
+}
+func TestValistPalindrome5(t *testing.T) {
+	result := validPalindrome("a")
+	answer := true
+	assert.Equal(t, result, answer)
+}
+func TestValistPalindrome6(t *testing.T) {
+	result := validPalindrome("aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga")
+	answer := true
+	assert.Equal(t, result, answer)
+}
