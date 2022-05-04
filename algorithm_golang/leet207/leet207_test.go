c84cbad647fdf525bd40daaec1c72a547ed045e3
Jinwoo Lee <starday456@kaist.ac.kr>
2022-05-04 19:31:50 +0900

    improve leet 207: topological sort

--- algorithm_golang/leet207/leet207_test.go
+++ algorithm_golang/leet207/leet207_test.go
@@ -23,7 +23,7 @@ func checkCycle(course int, dirGraph *[][]bool, visited *[]bool, currentSet *map
 	return false
 }
 
-func canFinish(numCourses int, prerequisites [][]int) bool {
+func solveByCheckingCycle(numCourses int, prerequisites [][]int) bool {
 	dirGraph := make([][]bool, numCourses)
 	visited := make([]bool, numCourses)
 	for i := range dirGraph {
@@ -55,6 +55,54 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 	return true
 }
 
+func solveByTopologicalSort(numCourses int, prerequisites [][]int) bool {
+	incomeCnt := make([]int, numCourses)
+	graph := make([][]int, numCourses)
+
+	for i := range graph {
+		graph[i] = []int{}
+		incomeCnt[i] = 0
+	}
+
+	for i := range prerequisites {
+		prev := prerequisites[i][0]
+		next := prerequisites[i][1]
+		graph[prev] = append(graph[prev], next)
+		incomeCnt[next]++
+	}
+
+	queue := []int{}
+	sorted := []int{}
+
+	for i := range incomeCnt {
+		if incomeCnt[i] == 0 {
+			queue = append(queue, i)
+		}
+	}
+
+	for ; len(queue) > 0; {
+		current := queue[0]
+		queue = queue[1:]
+		sorted = append(sorted, current)
+
+		nextCourses := graph[current]
+		for i := range nextCourses {
+			nextCourse := nextCourses[i]
+			incomeCnt[nextCourse]--
+			if incomeCnt[nextCourse] == 0 {
+				queue = append(queue, nextCourse)
+			}
+		}
+		graph[current] = []int{}
+	}
+	return len(sorted) == numCourses
+}
+
+func canFinish(numCourses int, prerequisites [][]int) bool {
+	// return solveByCheckingCycle(numCourses, prerequisites)
+	return solveByTopologicalSort(numCourses, prerequisites)
+}
+
 func TestCanFinish1(t *testing.T) {
 	result := canFinish(2, [][]int{{1,0}})
 	answer := true
@@ -64,5 +112,11 @@ func TestCanFinish1(t *testing.T) {
 func TestCanFinish2(t *testing.T) {
 	result := canFinish(2, [][]int{{1,0}, {0,1}})
 	answer := false
+	assert.Equal(t, answer, result)
+}
+
+func TestCanFinish3(t *testing.T) {
+	result := canFinish(2, [][]int{{0,1}})
+	answer := true
 	assert.Equal(t, answer, result)
 }
