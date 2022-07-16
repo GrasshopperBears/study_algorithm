2666d3078fa890cfa8abc6c018645d04dcecb3f8
Jinwoo Lee <starday456@kaist.ac.kr>
2022-07-16 17:11:46 +0900

    solve leetcode 559

---
+++ algorithm_java/leet559/src/Solution.java
@@ -0,0 +1,24 @@
+import leetLibrary.Node;
+import java.util.ArrayList;
+
+
+class Solution {
+    public int maxDepth(Node root) {
+        int answer = 0;
+        ArrayList<Node> q = new ArrayList<>();
+        q.add(root);
+        q.add(null);
+        while (true) {
+            Node curr = q.remove(0);
+            if (curr == null) break;
+
+            answer++;
+            while (curr != null) {
+                q.addAll(curr.children);
+                curr = q.remove(0);
+            }
+            q.add(null);
+        }
+        return answer;
+    }
+}
