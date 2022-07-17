4065e164dc00b1e9297c66d6a3a47379b047ce2f
Jinwoo Lee <starday456@kaist.ac.kr>
2022-07-18 04:02:51 +0900

    solve leetcode 2116 w/ stack - Check if a Parentheses String Can Be Valid

---
+++ algorithm_java/Leet2116.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+
+class Leet2116 {
+    public static boolean canBeValid(String s, String locked) {
+        Stack<Integer> lockedOpen = new Stack<>();
+        Stack<Integer> notLocked = new Stack<>();
+        int length = s.length();
+
+        if (length % 2 > 0) return false;
+
+        for (int i = 0; i < length; i++) {
+            if (locked.charAt(i) == '1') {
+                if (s.charAt(i) == ')') {
+                    if (lockedOpen.size() > 0) {
+                        lockedOpen.pop();
+                    } else if (notLocked.size() > 0) {
+                        notLocked.pop();
+                    } else {
+                        return false;
+                    }
+                    continue;
+                }
+                lockedOpen.push(i);
+            } else {
+                notLocked.push(i);
+            }
+        }
+
+        if (lockedOpen.size() > notLocked.size()) return false;
+        while (lockedOpen.size() > 0) {
+            if (lockedOpen.pop() > notLocked.pop()) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canBeValid("))()))", "010100")); // true
+        System.out.println(canBeValid("()()", "0000")); // true
+        System.out.println(canBeValid(")", "0")); // false
+        System.out.println(canBeValid("())()))()(()(((())(()()))))((((()())(())",
+                                    "1011101100010001001011000000110010100101")); // true
+    }
+}
