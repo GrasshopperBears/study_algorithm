0d56a86f317dba568f7a651ec6842f13fafe50ca
Jinwoo Lee <starday456@kaist.ac.kr>
2022-07-18 04:02:10 +0900

    solve leetcode 993 - Cousins in Binary Tree

---
+++ algorithm_java/Leet993.java
@@ -0,0 +1,25 @@
+import leetLibrary.TreeNode;
+
+class Leet993 {
+    int xDepth = -1, yDepth = -1, targetX, targetY, xParent, yParent;
+
+    private void visit(TreeNode node, int depth, int parent) {
+        if (xDepth >= 0 && yDepth >= 0) return;
+        if (node.val == targetX) {
+            xDepth = depth;
+            xParent = parent;
+        } else if (node.val == targetY) {
+            yDepth = depth;
+            yParent = parent;
+        }
+        if (node.right != null) visit(node.right, depth + 1, node.val);
+        if (node.left != null) visit(node.left, depth + 1, node.val);
+    }
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        targetX = x;
+        targetY = y;
+        visit(root, 0, 0);
+        return xDepth == yDepth && xParent != yParent;
+    }
+}
