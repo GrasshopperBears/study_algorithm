c11b7e783a9c2677889a71209c3344b2478192be
Jinwoo Lee <starday456@kaist.ac.kr>
2022-07-17 16:12:40 +0900

    change dp to HashMap

--- algorithm_java/leet823/src/Solution.java
+++ algorithm_java/leet823/src/Solution.java
@@ -3,27 +3,26 @@ import java.util.stream.Collectors;
 
 class Solution {
     public static int numFactoredBinaryTrees(int[] arr) {
-        long[] dp = new long[arr.length];
+        long answer = 0;
         Set<Integer> set = Arrays.stream(arr).boxed().collect(Collectors.toSet());
-        HashMap<Integer, Integer> indices = new HashMap<>();
+        HashMap<Integer, Long> dp = new HashMap<>();
         Arrays.sort(arr);
 
-        for (int i = 0; i < arr.length; i++) {
-            int num = arr[i];
-            int bound = (int)Math.round(Math.sqrt(num));
-            indices.put(arr[i], i);
-            dp[i] = 1;
+        for (int num : arr) {
+            int bound = (int) Math.round(Math.sqrt(num));
+            dp.put(num, (long) 1);
             for (int factor = 2; factor <= bound; factor++) {
                 if (num % factor > 0) continue;
                 int div = num / factor;
                 if (set.contains(factor) && set.contains(div)) {
-                    long childPossible = dp[indices.get(factor)] * dp[indices.get(div)];
+                    long childPossible = dp.get(factor) * dp.get(div);
                     if (div != factor) childPossible *= 2;
-                    dp[i] += childPossible;
+                    dp.put(num, dp.get(num) + childPossible);
                 }
             }
+            answer = (answer + dp.get(num)) % 1000000007;
         }
-        return (int)(Arrays.stream(dp).sum() % 1000000007);
+        return (int)answer;
     }
 
     public static void main(String[] args) {
