9039f186cd27fe4c709bfd4779e717dad5299a61
Jinwoo Lee <starday456@kaist.ac.kr>
2022-09-04 19:25:39 +0900

    improve w/bfs

--- algorithm_java/programmers/hikingCourse/Solution.java
+++ algorithm_java/programmers/hikingCourse/Solution.java
@@ -17,50 +17,22 @@ class Path implements Comparable<Path> {
 }
 
 class Solution {
-    private static int[] parents, heights;
     private static boolean[] visited;
     private static ArrayList<Integer>[] graph;
-    private static HashSet<Integer> summitSet = new HashSet<>();
+    private static HashSet<Integer> summitSet = new HashSet<>(), gateSet = new HashSet<>();
 
-    private static int findParent(int p) {
-        if (parents[p] == p)
-            return p;
-        parents[p] = findParent(parents[p]);
-        return parents[p];
-    }
-
-    private static void union(int p1, int p2) {
-        p1 = findParent(p1);
-        p2 = findParent(p2);
-        if (p1 == p2)
-            return;
-        if (heights[p1] < heights[p2]) {
-            parents[p1] = p2;
-        } else {
-            parents[p2] = p1;
-            if (heights[p1] == heights[p2])
-                heights[p1]++;
-        }
-    }
-
-    private static int findSummit(int gate) {
-        int minSummit = Integer.MAX_VALUE;
+    private static int findGate(int summit) {
         Queue<Integer> q = new LinkedList<>();
-        q.add(gate);
-        visited[gate] = true;
-        q.add(0);
+        Arrays.fill(visited, false);
+        q.add(summit);
+        visited[summit] = true;
 
-        while (true) {
+        while (q.size() > 0) {
             int pos = q.poll();
-            if (pos == 0) {
-                if (q.size() == 0)
-                    break;
-                q.add(0);
-            }
-            if (summitSet.contains(pos)) {
-                minSummit = Math.min(minSummit, pos);
+            if (gateSet.contains(pos))
+                return pos;
+            if (summitSet.contains(pos) && pos != summit)
                 continue;
-            }
             ArrayList<Integer> adjList = graph[pos];
             if (adjList == null)
                 continue;
@@ -71,7 +43,7 @@ class Solution {
                 }
             }
         }
-        return minSummit;
+        return 0;
     }
 
     public static int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
@@ -80,23 +52,19 @@ class Solution {
 
         graph = new ArrayList[n + 1];
         visited = new boolean[n + 1];
-        parents = new int[n + 1];
-        heights = new int[n + 1];
 
         for (int summit : summits)
             summitSet.add(summit);
-
-        for (int i = 1; i <= n; i++)
-            parents[i] = i;
+        for (int gate : gates)
+            gateSet.add(gate);
 
         for (int[] path : paths)
             pq.add(new Path(path[0], path[1], path[2]));
 
         while (pq.size() > 0) {
             int minPathCost = pq.peek().cost;
-            while (pq.peek().cost == minPathCost) {
+            while (pq.size() > 0 && pq.peek().cost == minPathCost) {
                 Path minPath = pq.poll();
-                union(minPath.p1, minPath.p2);
                 if (graph[minPath.p1] == null)
                     graph[minPath.p1] = new ArrayList<>();
                 if (graph[minPath.p2] == null)
@@ -105,12 +73,11 @@ class Solution {
                 graph[minPath.p2].add(minPath.p1);
             }
             for (int summit : summitSet) {
-                for (int gate = 0; gate < gates.length; gate++) {
-                    if (findParent(gates[gate]) == findParent(summit)) {
-                        answer[0] = findSummit(gates[gate]);
-                        answer[1] = minPathCost;
-                        return answer;
-                    }
+                int gate = findGate(summit);
+                if (gate > 0) {
+                    answer[0] = summit;
+                    answer[1] = minPathCost;
+                    return answer;
                 }
             }
         }
@@ -123,10 +90,10 @@ class Solution {
         // { 4, 5, 3 }, { 4, 6, 1 }, { 5, 6, 1 } }, new int[] { 1, 3 },
         // new int[] { 5
         // }));
-        // System.out.println(solution(7,
-        // new int[][] { { 1, 4, 4 }, { 1, 6, 1 }, { 1, 7, 3 }, { 2, 5, 2 }, { 3, 7, 4
-        // }, { 5, 6, 6 } },
-        // new int[] { 1 }, new int[] { 2, 3, 4 }));
+        System.out.println(solution(7,
+                new int[][] { { 1, 4, 4 }, { 1, 6, 1 }, { 1, 7, 3 }, { 2, 5, 2 }, { 3, 7, 4
+                }, { 5, 6, 6 } },
+                new int[] { 1 }, new int[] { 2, 3, 4 }));
         // System.out.println(solution(7, new int[][] { { 1, 2, 5 }, { 1, 4, 1 }, { 2,
         // 3, 1 }, { 2, 6, 7 }, { 4, 5, 1 },
         // { 5, 6, 1 }, { 6, 7, 1 } }, new int[] { 3, 7 }, new int[] { 1, 5 }));
