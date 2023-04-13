38c20904f2e879c1558ee5139f9ee4f82d553258
GrasshopperBears <starday456@naver.com>
2023-04-13 22:28:54 +0900

    (another solution) boj11444 with higher-order function

--- algorithm_kotlin/src/main/kotlin/boj11444/Main.kt
+++ algorithm_kotlin/src/main/kotlin/boj11444/Main.kt
@@ -1,21 +1,32 @@
 package boj11444
 
 val BASE = 1000000007
-val A = arrayOf(longArrayOf(1, 1), longArrayOf(1, 0))
-val dp = HashMap<Long, Array<LongArray>?>()
+val A = arrayOf(intArrayOf(1, 1), intArrayOf(1, 0))
+val dp = HashMap<Long, Array<IntArray>?>()
 
-fun multiply(arr1: Array<LongArray>, arr2: Array<LongArray>): Array<LongArray> {
-    val result = arrayOf(longArrayOf(0, 0), longArrayOf(0, 0))
-
-    result[0][0] = ((arr1[0][0] * arr2[0][0]) % BASE + (arr1[0][1] * arr2[1][0]) % BASE) % BASE
-    result[0][1] = ((arr1[0][0] * arr2[0][1]) % BASE + (arr1[0][1] * arr2[1][1]) % BASE) % BASE
-    result[1][0] = ((arr1[1][0] * arr2[0][0]) % BASE + (arr1[1][1] * arr2[1][0]) % BASE) % BASE
-    result[1][1] = ((arr1[1][0] * arr2[0][1]) % BASE + (arr1[1][1] * arr2[1][1]) % BASE) % BASE
-
-    return result
+fun Array<IntArray>.toLongArray(): List<List<Long>> {
+    return this.map { r -> r.map { x -> x.toLong() } }
 }
 
-fun A_n(n: Long): Array<LongArray> {
+fun Array<LongArray>.toLongArray(): Array<IntArray> {
+    return this.map { r -> r.map { x -> x.toInt() }.toIntArray() }.toTypedArray()
+}
+
+fun multiply(left: Array<IntArray>, right: Array<IntArray>): Array<IntArray> {
+    val result = arrayOf(longArrayOf(0, 0), longArrayOf(0, 0))
+
+    val arr1 = left.toLongArray()
+    val arr2 = right.toLongArray()
+
+    result[0][0] = (arr1[0][0] * arr2[0][0] + arr1[0][1] * arr2[1][0]) % BASE
+    result[0][1] = (arr1[0][0] * arr2[0][1] + arr1[0][1] * arr2[1][1]) % BASE
+    result[1][0] = (arr1[1][0] * arr2[0][0] + arr1[1][1] * arr2[1][0]) % BASE
+    result[1][1] = (arr1[1][0] * arr2[0][1] + arr1[1][1] * arr2[1][1]) % BASE
+
+    return result.toLongArray()
+}
+
+fun A_n(n: Long): Array<IntArray> {
     val cache = dp.getOrDefault(n, null)
     if (cache != null) {
         return cache
